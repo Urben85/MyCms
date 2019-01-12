d97749fd7fb41e5145f7338b3908bd4411f65221
Urben <37121826+Urben85@users.noreply.github.com>
2019-01-12 16:40:15 +0100

    ...

--- src/apps/admin/admin.js
+++ src/apps/admin/admin.js
@@ -8,5 +8,36 @@ import $ from 'jquery';
 
 // Document Ready Event
 $(document).ready(() => {
-    console.log('works');
-});
+    // Init Navigation
+    Navigation();
+    // Load Updates on load
+    $('#UPDATES').click();
+})
+
+function Navigation() {
+    $(document).on('click','.w3-bar-item',(e) => {
+        var navId = ($(e.target).attr('id')) ? $(e.target).attr('id') : $(e.target).parent().attr('id');
+        var areaId = navId + '_Area';
+        // Handle NavItem
+        $('#' + navId).removeClass('w3-hover-black').addClass('w3-black');
+        if ($('#' + navId).hasClass('MainNav')) {
+            $('.MainNav').removeClass('w3-black');
+            $('#' + navId).addClass('w3-black');
+        }
+        // Handle Content
+        $('.w3-content').hide();
+        $('#' + areaId).fadeIn(500);
+        // Load Areas
+        switch(navId) {
+            case 'UPDATES': {
+                break;
+            }
+            case 'MODELS': {
+                break;
+            }
+            case 'SETTINGS': {
+                break;
+            }
+        }
+    })
+}
